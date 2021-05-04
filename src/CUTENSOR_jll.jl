4e40776af0958aecc4c49dc0f55e01a026ef9bc6
jlbuild <juliabuildbot@gmail.com>
2021-05-04 20:43:59 +0000

    CUTENSOR_jll build 1.3.0+0

---
+++ src/CUTENSOR_jll.jl
@@ -0,0 +1,10 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule CUTENSOR_jll
+using Base
+using Base: UUID
+using LazyArtifacts
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("CUTENSOR")
+JLLWrappers.@generate_main_file("CUTENSOR", UUID("35b6c64b-1ee1-5834-92a3-3f624899209a"))
+end  # module CUTENSOR_jll
