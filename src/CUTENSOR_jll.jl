bd9ad061f8aa363021bd7ba5b83d73b8b389c452
jlbuild <juliabuildbot@gmail.com>
2026-01-05 13:37:33 +0000

    CUTENSOR_jll build 2.4.1+0

--- src/CUTENSOR_jll.jl
+++ src/CUTENSOR_jll.jl
@@ -7,5 +7,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("CUTENSOR")
-JLLWrappers.@generate_main_file("CUTENSOR", UUID("35b6c64b-1ee1-5834-92a3-3f624899209a"))
+JLLWrappers.@generate_main_file("CUTENSOR", Base.UUID("35b6c64b-1ee1-5834-92a3-3f624899209a"))
 end  # module CUTENSOR_jll
